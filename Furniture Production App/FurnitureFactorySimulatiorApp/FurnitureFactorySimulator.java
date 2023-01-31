0dbe7d24b7f78fdc1dd554f49df023aa6472df8e
Berat Altuntaş <73036762+berataltuntash@users.noreply.github.com>
2023-01-31 15:04:15 +0300

    Add files via upload

---
+++ Furniture Production App/FurnitureFactorySimulatiorApp/FurnitureFactorySimulator.java
@@ -0,0 +1,10 @@
+package FurnitureFactorySimulatorApp;
+import java.io.FileNotFoundException;
+
+import FurnitureFactorySimulator.ProgramExecuter; 
+
+class FurnitureFactorySimulator {
+	public static void main(String args[]) throws FileNotFoundException {
+		new ProgramExecuter();
+	}
+}
